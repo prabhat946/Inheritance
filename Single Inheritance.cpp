0c3c47d0a3fb19363b462babfd266f8b8bd9db34
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-10-07 12:59:47 +0530

    Single Inheritance.cpp

---
+++ Single Inheritance.cpp
@@ -0,0 +1,34 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Inheritance to perform Single Inheritance
+
+#include <iostream>
+using namespace std;
+
+class Uni {
+public:
+    string uni = "Symbiosis";
+    void discipline() {
+        cout << "Engineering" << endl;
+    }
+};
+
+class Dep : public Uni {
+public:
+    string dept = "Electronics & Communication";
+};
+
+int main() {    
+    Dep department;
+    cout << department.uni << endl;
+    department.discipline();
+    cout << department.dept << endl;
+
+    return 0;
+}
+
+
+/*Output:
+Symbiosis
+Engineering
+Electronics & Communication*/
