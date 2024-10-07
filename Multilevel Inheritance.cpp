6c6bb157d18566b89723813da230d00e245bc5d5
prabhat946 <chakravarthula.prabhat.btech2023@sitpune.edu.in>
2024-10-07 12:58:20 +0530

    Multilevel Inheritance.cpp

---
+++ Multilevel Inheritance.cpp
@@ -0,0 +1,42 @@
+//Name:prabhat
+//PRN:23070123040
+//Aim:Implemention of Inheritance to perform Multilevel Inheritance
+
+#include <iostream>
+using namespace std;
+
+class Food {
+public:
+    string cuisine = "Indian";
+    void type() {
+        cout << "Asian" << endl;
+    }
+};
+
+class Dish : public Food {
+public:
+    string dish = "Biriyani";
+};
+
+class Restaurant : public Dish {
+public:
+    string name = "Spice Kitchen";
+};
+
+int main() {
+    Restaurant myRestaurant;
+    cout << "Restaurant Name: " << myRestaurant.name << endl;
+    cout << "Cuisine: " << myRestaurant.cuisine << endl;
+    cout << "Dish: " << myRestaurant.dish << endl;
+    cout << "Type of cuisine: ";
+    myRestaurant.type();
+
+    return 0;
+}
+
+
+/*Output:
+Restaurant Name: Spice Kitchen
+Cuisine: Indian
+Dish: Biriyani
+Type of cuisine: Asian*/
